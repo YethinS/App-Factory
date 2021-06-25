506696419f37173fff509eff2913f4f10b729bfd
yethin <s.yethin@gmail.com>
2021-06-25 20:12:04 +0530

    Second commit including some of the working js events

--- js/icon-generator.js
+++ js/icon-generator.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $('#btn-trim, #btn-dtrim, #btn-crop, #btn-center, #btn-none, #btn-elevate, #btn-cast, #btn-score').click(function(){
-        $('#btn-trim, #btn-dtrim, #btn-crop, #btn-center, #btn-none, #btn-elevate, #btn-cast, #btn-score').removeClass('active');
+    $('#btn-none, #btn-elevate, #btn-cast, #btn-score').click(function(){
+        $('#btn-none, #btn-elevate, #btn-cast, #btn-score').removeClass('active');
         $(this).addClass('active');
     });
 });
@@ -18,6 +18,20 @@ $(document).ready(function(){
         $('#btn-take, #btn-ask, #btn-trade').removeClass('active');
         $(this).addClass('active');
     });
+});
+//Trim button group color change
+$(document).ready(function(){
+  $('#btn-trim, #btn-dtrim').click(function(){
+      $('#btn-trim, #btn-dtrim').removeClass('active');
+      $(this).addClass('active');
+  });
+});
+//Crop button group color change
+$(document).ready(function(){
+  $('#btn-crop, #btn-center').click(function(){
+      $('#btn-crop, #btn-center').removeClass('active');
+      $(this).addClass('active');
+  });
 });
 
 //Icon display.
@@ -57,11 +71,38 @@ document.getElementById("icon-1").addEventListener("click", function() {
   
   });
 
-  //Display js code test 2//
+  //Display icon to right side//
 
   function myFunction(i) {
     var icon_2 = document.querySelector("#icon-2");
     icon_2.className = i.className
 }
 
-  
+//Trim js code test//
+function myTrim() {
+  var element = document.getElementById("box");
+  element.classList.add("active");
+}
+function trim(){
+  var element = document.getElementById("box");
+  element.classList.remove("active");
+}
+//Color change
+function colorChange(){
+  let color = document.getElementById("favcolor").value;
+  var icon_color = document.getElementById("icon-2");
+  console.log(color);
+  icon_color.style.color = color;
+}
+//background-color change
+function bcolorChange(){
+  let color = document.getElementById("b-color").value;
+  console.log(color);
+  var bcolor = document.getElementById("box");
+  bcolor.style.background = color;
+}
+//default color palette value
+function default_color(){
+  document.getElementById("favcolor").value = '#0000ff';
+  document.getElementById("b-color").value = '#5192ff';
+}
